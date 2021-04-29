b4f7db5772dc1f72ea36878fb9e972500720c958
jeff0brewer-cs130 <jeff00brewer@gmail.com>
2021-04-29 16:07:47 -0600

    remove arg

--- tutorial05/font-switcher/js/index.js
+++ tutorial05/font-switcher/js/index.js
@@ -1,12 +1,12 @@
 const content = document.querySelector('.content');
 
 const makeBigger = () => {
-   const curr_size = window.getComputedStyle(content, null).getPropertyValue('font-size');
+   const curr_size = window.getComputedStyle(content).getPropertyValue('font-size');
    content.style.fontSize = parseInt(curr_size.replace("px", "")) + 3 + "px";
 };
 
 const makeSmaller = () => {
-   const curr_size = window.getComputedStyle(content, null).getPropertyValue('font-size');
+   const curr_size = window.getComputedStyle(content).getPropertyValue('font-size');
    let int_size = parseInt(curr_size.replace("px", "")) - 3;
    int_size = int_size < 0 ? 0 : int_size;
    content.style.fontSize =  int_size + "px";
