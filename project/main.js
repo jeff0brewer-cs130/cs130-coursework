bcc47a470e7b51e36127b3f4116955001510eacd
jeff0brewer-cs130 <jeff00brewer@gmail.com>
2021-05-27 15:37:09 -0600

    fetch token test

--- project/main.js
+++ project/main.js
@@ -1,3 +1,7 @@
 const url_search = new URLSearchParams(window.location.search);
 const auth_code = url_search.get('code');
-console.log(auth_code);
+
+fetch(`https://www.bungie.net/Platform/App/OAuth/Token?grant_type=authorization_code&code=${auth_code}`, { method: 'POST' })
+    .then(res => {
+        console.log(res);
+    });
