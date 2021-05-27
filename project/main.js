f6a9807431678820948eaba3b46e626d15ae9dd3
jeff0brewer-cs130 <jeff00brewer@gmail.com>
2021-05-27 17:17:44 -0600

    add body

--- project/main.js
+++ project/main.js
@@ -1,13 +1,14 @@
 const url_search = new URLSearchParams(window.location.search);
 const auth_code = url_search.get('code');
 const baseURL = 'http://localhost:8081';
+const imageURL = 'http://bungie.net';
 
 const init = async () => {
     let authorize = await fetch(`${baseURL}/authorize/${auth_code}`);
     if(authorize.ok){
         console.log('login success');
         let char_info = await fetch(`${baseURL}/getProfile/Characters`);
-        console.log(char_info);
+        console.log(char_info.body);
     }
     else{
         console.log('login failed');
