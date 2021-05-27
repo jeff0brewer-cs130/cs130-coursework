042efc9a91f7c95eba772ba1cfdb65a5bb0cfdf7
jeff0brewer-cs130 <jeff00brewer@gmail.com>
2021-05-27 17:58:22 -0600

    char event handler

--- project/main.js
+++ project/main.js
@@ -1,7 +1,7 @@
 const url_search = new URLSearchParams(window.location.search);
 const auth_code = url_search.get('code');
 const baseURL = 'http://localhost:8081';
-const imageURL = 'http://bungie.net';
+const imageURL = 'https://bungie.net';
 
 const bungieEnum = {
     classType: ["Titan", "Hunter", "Warlock"]
@@ -14,17 +14,26 @@ const init = async () => {
         let char_info = await fetch(`${baseURL}/getProfile/Characters`);
         char_info = await char_info.json();
         char_info = char_info.characters.data;
-        console.log(char_info);
         char_elem = document.querySelectorAll('.character');
         for(let i = 0; i < 3; i++){
             let curr_id = Object.keys(char_info)[i];
             char_elem[i].setAttribute('data-char_id', curr_id);
             char_elem[i].style.backgroundImage = `url(${imageURL + char_info[curr_id].emblemBackgroundPath})`;
             char_elem[i].innerHTML = bungieEnum.classType[char_info[curr_id].classType];
+            char_elem[i].onclick = get_char_inventory;
         }
     }
     else{
         console.log('login failed');
+        window.location.href = "./login.html";
     }
-}
+};
 init();
+
+const get_char_inventory = async (ev) => {
+    let char_inventory = await fetch(`${baseURL}/getProfile/CharacterInventories`);
+    char_inventory = await char_inventory.json();
+    let char_id = ev.target.dataset.char_id;
+    console.log(char_inventory);
+    console.log(char_id);
+};
