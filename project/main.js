872e720dabd82a5766e39213b7494bd499022659
jeff0brewer-cs130 <jeff00brewer@gmail.com>
2021-05-27 17:14:53 -0600

    char info testing

--- project/main.js
+++ project/main.js
@@ -1,7 +1,16 @@
 const url_search = new URLSearchParams(window.location.search);
 const auth_code = url_search.get('code');
+const baseURL = 'http://localhost:8081';
 
-fetch(`http://localhost:8081/authorize/${auth_code}`)
-    .then(res => {
-        console.log(res);
-    });
+const init = async () => {
+    let authorize = await fetch(`${baseURL}/authorize/${auth_code}`);
+    if(authorize.ok){
+        console.log('login success');
+        let char_info = await fetch(`${baseURL}/getProfile/Characters`);
+        console.log(char_info);
+    }
+    else{
+        console.log('login failed');
+    }
+}
+init();
--- project/src/routes.js
+++ project/src/routes.js
@@ -1,40 +1,78 @@
 require('dotenv').config();
 const axios = require('axios');
+const { response } = require('express');
 const express = require("express");
 const router = express.Router();
 
 axios.defaults.headers.common['X-API-Key'] = process.env.BUNGIE_API_KEY;
 
-const OAuth = {
+let OAuth = {
     client_id: '36633',
     client_secret: 'wtb8SXXP-uTQY3xLDhQw3vML-6hjxd881X3sCiRQGYE'
-}
+};
+
+let user = {};
 
 const encodebase64 = str => {
-    let buff = new Buffer(str);
+    let buff = Buffer.from(str);
     return buff.toString('base64');
 };
+
+const bungieURL = 'https://www.bungie.net/Platform';
 
 router.route("/authorize/:auth_code")
     .get((req, res) => {
         console.log(`GET /authorize/${req.params.auth_code}`);
-        axios.post('https://www.bungie.net/Platform/App/OAuth/Token/', `grant_type=authorization_code&code=${auth_code}`,
+        axios.post(`${bungieURL}/App/OAuth/Token/`, `grant_type=authorization_code&code=${req.params.auth_code}`,
             {
                 headers: {
                     "Authorization": `Basic ${encodebase64(`${OAuth.client_id}:${OAuth.client_secret}`)}`,
                     "Content-Type": "application/x-www-form-urlencoded"
                 }
             })
-            .then(res => {
-                OAuth.access_token = res.data.access_token;
-                console.log(OAuth.access_token);
-                res.status(200).send("authorization successful");
+            .then(response => {
+                OAuth.access_token = response.data.access_token;
+                axios.get(`${bungieURL}/User/GetMembershipsForCurrentUser/`, {
+                        headers: {
+                            Authorization: 'Bearer ' + OAuth.access_token
+                        }
+                    })
+                    .then(response => {
+                        const destiny_info = response.data.Response.destinyMemberships[0];
+                        console.log(destiny_info);
+                        user.username = destiny_info.displayName;
+                        user.member_id = destiny_info.membershipId;
+                        user.member_type = destiny_info.membershipType;
+                        res.status(200).send();
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        res.status(500).send();
+                    })
             })
             .catch(err => {
                 console.log(err);
-                res.status(500).send("authorization failed");
+                res.status(500).send();
             });
     });
+
+router.route("/getProfile/:components")
+    .get((req, res) => {
+        console.log(`GET /getProfile/${req.params.components}`);
+
+        axios.get(`${bungieURL}/Destiny2/${user.member_type}/Profile/${user.member_id}?components=${req.params.components}`, {
+                headers: {
+                    Authorization: 'Bearer ' + OAuth.access_token
+                }
+            })
+            .then(response => {
+                res.status(200).send(JSON.stringify(response.data.Response));
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).send();
+            })
+    })
 
 
 
