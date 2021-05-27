8c41f377e75c7bf9b1c8590f4f6d20fd9b16d87d
jeff0brewer-cs130 <jeff00brewer@gmail.com>
2021-05-27 17:31:19 -0600

    add emblem char bg

--- project/main.js
+++ project/main.js
@@ -3,13 +3,25 @@ const auth_code = url_search.get('code');
 const baseURL = 'http://localhost:8081';
 const imageURL = 'http://bungie.net';
 
+let user = {
+    char_ids = []
+};
+
 const init = async () => {
     let authorize = await fetch(`${baseURL}/authorize/${auth_code}`);
     if(authorize.ok){
         console.log('login success');
         let char_info = await fetch(`${baseURL}/getProfile/Characters`);
         char_info = await char_info.json();
+        char_info = char_info.characters.data;
         console.log(char_info);
+        char_elem = document.querySelectorAll('.character');
+        for(let i = 0; i < char_info.length; i++){
+            let curr_id = Object.keys(char_info)[i];
+            user.char_ids.push(curr_id);
+            char_elem[i].style.backgroundImage = `url(${imageURL + char_info[curr_id].emblemBackgroundPath})`;
+        }
+
     }
     else{
         console.log('login failed');
