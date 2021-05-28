11fc6ab1932c199d389ccea3c3a5f5224e239c1e
jeff0brewer-cs130 <jeff00brewer@gmail.com>
2021-05-27 18:17:08 -0600

    test query

--- project/main.js
+++ project/main.js
@@ -4,8 +4,29 @@ const baseURL = 'http://localhost:8081';
 const imageURL = 'https://bungie.net';
 
 const bungieEnum = {
-    classType: ["Titan", "Hunter", "Warlock"]
-}
+    classType: ["Titan", "Hunter", "Warlock"],
+    bucket: {
+        kinetic: 1498876634,
+        energy: 2465295065,
+        power: 953998645,
+        head: 3448274439,
+        arms: 3551918588,
+        chest: 14239492,
+        legs: 20886954,
+        classitem: 158578786
+    }
+};
+
+let inv_index = {
+    kinetic: 0,
+    energy: 0,
+    power: 0,
+    head: 0,
+    arms: 0,
+    chest: 0,
+    legs: 0,
+    classitem: 0
+};
 
 const init = async () => {
     let authorize = await fetch(`${baseURL}/authorize/${auth_code}`);
@@ -25,15 +46,22 @@ const init = async () => {
     }
     else{
         console.log('login failed');
-        window.location.href = "./login.html";
+        // window.location.href = "./login.html";
     }
 };
 init();
 
 const get_char_inventory = async (ev) => {
-    let char_inventory = await fetch(`${baseURL}/getProfile/CharacterInventories`);
+    let char_inventory = await fetch(`${baseURL}/getProfile/CharacterInventories,ItemInstances`);
     char_inventory = await char_inventory.json();
-    let char_id = ev.target.dataset.char_id;
     console.log(char_inventory);
-    console.log(char_id);
+    char_inventory = char_inventory.characterInventories.data[ev.target.dataset.char_id].items;
+    // char_inventory.forEach(item => {
+    //     switch(item.bucketHash){
+    //         case bungieEnum.bucket.kinetic:
+    //             document.querySelectorAll('.kinetic')[1].querySelectorAll('div');
+    //             break;
+    //     }
+    // });
+    console.log(char_inventory);
 };
